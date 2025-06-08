b9ee9dbf41a2d8bc0f6e32db468e71b63455acbd
Alvaro Mucida <alvaromucidaac@gmail.com>
2025-06-08 19:31:30 -0300

    fix: Corrigi a Classe JwtUtil

--- src/main/java/com/mucida/usuario/infrastructure/security/JwtUtil.java
+++ src/main/java/com/mucida/usuario/infrastructure/security/JwtUtil.java
@@ -7,29 +7,26 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
 public class JwtUtil {
 
-    private final SecretKey secretKey;
-
-    public JwtUtil() {
-        this.secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-    }
+    private final String secretKey = "sua-chave-secreta-super-segura-que-deve-ser-bem-longa";
 
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public Claims extractClaims(String token) {
         return Jwts.parser()
-                .setSigningKey(secretKey)
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
